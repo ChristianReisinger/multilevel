ba395f58ec2c0b384cc43fa6dd4e6f7ea8b6b5e3
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-08-07 16:21:47 +0200

    Full revamp of algorithm.
    * In each step, compute sublattice operators on the full lattice
    (instead of repeating for each site < reads each config many times < too
    slow!!)

--- src/multilevel.cc
+++ src/multilevel.cc
@@ -18,6 +18,7 @@
 #include <helper_functions.hh>
 
 #include <sublattice_algebra.hh>
+#include <twolink_operators.hh>
 
 //compute [ [T(0)T(1)] [T(2)T'(3)] ] [ [T''(4)T(5)] [T(6)T(7)] ]
 //with T' T'' such that E_z is inserted at t=4 as a clover
@@ -35,114 +36,120 @@ std::string config_filename(const std::vector<int>& tag) {
 	return filename_oss.str();
 }
 
-/**
- * n : starting lattice site of the full operator
- * t_sub : initial time of the timeslice on which to compute the sublattice operator
- */
-void sublattice_operator(const std::vector<int>& conf_tag, int t_sub, const std::array<int, 4>& n, int rsep, int dir, double* ret) {
+unsigned long int T_field_index(int t, int x, int y, int z, int n, int i, int T, int L, int timeslice_thickness) {
+	return (n * get_index(t / timeslice_thickness, x, y, z, T / timeslice_thickness, L) + i) * SO_elems;
+}
 
-	double* sub_gauge_field;
+void T_field_alloc_zero(double* T_field, int n, int timeslice_num, int L) {
+	T_field = new double[n * SO_elems * L * L * L * timeslice_num]();
+}
+
+void T_field_di_eq_re(double* T_field, double re, int n, int T, int L, int timeslice_thickness) {
+	for (int i = 0; i < n * SO_elems * L * L * L * T / timeslice_thickness; ++i)
+		T_field[i] /= re;
+}
+
+void obtain_sublattice_gauge_field(double* sub_gauge_field, const std::vector<int>& tag, int T, int L) {
 	Gauge_Field_Alloc(&sub_gauge_field, T, L);
-	read_gauge_field(sub_gauge_field, config_filename(conf_tag).c_str(), T, L);
-
-	double T0[SUN_elems], TR[SUN_elems];
-	LinkPath p(sub_gauge_field, T, L, n);
-	cm_eq_cm(T0, p.move(0, t_sub)(0, true)(0, true).path);
-	cm_eq_cm(TR, p.reset(n).move(0, t_sub).move(dir, rsep)(0, true)(0, true).path);
-
-	if (insert_E) {
-		double clov[SUN_elems], clov_dag[SUN_elems], U[SUN_elems];
-		if (t_sub == 2) {
-			cm_eq_cm(clov, p.reset(n).move(0, t_sub + 2).move(dir, rsep)(dir, true)(0, false)(dir, false)(0, true).path);
-			cm_pl_eq_cm(clov, p.reset(n).move(0, t_sub + 2).move(dir, rsep)(0, false)(dir, false)(0, true)(dir, true).path);
-
-			cm_eq_cm_dag(clov_dag, clov);
-			cm_ti_eq_re(clov_dag, -1);
-			cm_pl_eq_cm(clov, clov_dag);
-			cm_ti_eq_re(clov, 0.5);
-
-			cm_eq_cm_ti_cm(U, TR, clov);
-			cm_eq_cm(TR, U);
-		} else if (t_sub == 4) {
-			cm_eq_cm(clov, p.reset(n).move(0, t_sub).move(dir, rsep)(0, true)(dir, true)(0, false)(dir, false).path);
-			cm_pl_eq_cm(clov, p.reset(n).move(0, t_sub).move(dir, rsep)(dir, false)(0, true)(dir, true)(0, false).path);
-
-			cm_eq_cm_dag(clov_dag, clov);
-			cm_ti_eq_re(clov_dag, -1);
-			cm_pl_eq_cm(clov, clov_dag);
-			cm_ti_eq_re(clov, 0.5);
-
-			cm_eq_cm_ti_cm(U, clov, TR);
-			cm_eq_cm(TR, U);
-		}
-	}
-
-	so_eq_cm_x_cm(ret, T0, TR);
-}
-void multilevel(const std::vector<int>& conf_tag, int WL_T, int level,
-		int t_offset, const std::array<int, 4>& n, int dir, int rsep,
-		double* const ret) {
-
-	const std::vector<int> level_thickness { WL_T, 4, 2 };
-
-	so_eq_zero(ret);
-	for (int conf = 1; conf <= level_config_num[level]; ++conf) {
-		std::vector<int> curr_tag(conf_tag);
-		if (level != 0)
-			curr_tag.push_back(conf);
-
-		double curr_so[SO_elems];
-
-		if (level < 2) {
-			so_eq_id(curr_so);
-			for (int t_sub = 0; t_sub < level_thickness[level]; t_sub += level_thickness[level + 1]) {
-				double subavg[SO_elems], U[SO_elems];
-				multilevel(curr_tag, WL_T, level + 1, t_offset + t_sub, n, dir, rsep, subavg);
-				so_eq_so_ti_so(U, curr_so, subavg);
-				so_eq_so(curr_so, U);
-			}
-		} else {
-			sublattice_operator(curr_tag, t_offset, n, rsep, dir, curr_so);
-		}
-
-		so_pl_eq_so(ret, curr_so);
-	}
-	so_ti_eq_re(ret, 1.0 / level_config_num[level]);
+	read_gauge_field(sub_gauge_field, config_filename(tag).c_str(), T, L);
 }
 
-/**
- * compute the Wilson loop from the sublattice operator SO and spatial Wilson lines
- * S0 at t = 0, and ST at t = T = temporal Wilson loop size
- */
-complex complete_Wilson_loop(const double* SO, const double* S0, const double* ST) {
-	double S0_ti_T[SUN_elems];
-	cm_eq_zero(S0_ti_T);
+void multilevel(const std::vector<int>& conf_tag, const std::vector<int>& level_thickness, int level, int WL_R, double** T_fields) {
+	if (level == 0) {
+		double* level1_T_fields[2];
+		for (int i = 0; i < 2; ++i)
+			T_field_alloc_zero(level1_T_fields[i], 3, T / level_thickness[1], L);
+		multilevel(conf_tag, level_thickness, level + 1, WL_R, level1_T_fields);
 
-	for (int a = 0; a < SUN_N; ++a) {
-		for (int b = 0; b < SUN_N; ++b) {
-			for (int i = 0; i < SUN_N; ++i) {
-				for (int j = 0; j < SUN_N; ++j) {
-					const double* S0_elem_ptr = S0 + cm_superindex(i, j);
-					complex S0_elem { *S0_elem_ptr, *(S0_elem_ptr) };
-					const double* T_elem_ptr = SO + so_superindex(i, a, j, b);
-					complex T_elem { *T_elem_ptr, *(T_elem_ptr) };
+		for (int t = 0; t < level_thickness[0]; t += level_thickness[1]) {
+			for (int x = 0; x < L; ++x) {
+				for (int y = 0; y < L; ++y) {
+					for (int z = 0; z < L; ++z) {
+						for (int i = 0; i < 3; ++i) {
+							int sub_index_first = T_field_index(t, x, y, z, 3, i, T, L, level_thickness[1]);
+							int sub_index_second = T_field_index(t + level_thickness[level + 1], x, y, z, 3, i, T, L, level_thickness[1]);
 
-					double* S0_ti_T_elem_ptr = S0_ti_T + cm_superindex(a, b);
-					complex S0_ti_T_elem;
-					co_eq_co_ti_co(&S0_ti_T_elem, &S0_elem, &T_elem);
-					*S0_ti_T_elem_ptr += S0_ti_T_elem.re;
-					*(S0_ti_T_elem_ptr + 1) += S0_ti_T_elem.im;
+							so_eq_so_ti_so(*T_fields + sub_index_first,
+									level1_T_fields[0] + sub_index_first,
+									level1_T_fields[1] + sub_index_second);
+						}
+					}
 				}
 			}
 		}
-	}
-	double S0_ti_T_ti_ST[SUN_elems];
-	cm_eq_cm_ti_cm(S0_ti_T_ti_ST, S0_ti_T, ST);
+		for (int i = 0; i < 2; ++i)
+			delete[] level1_T_fields[i];
 
-	complex WL;
-	co_eq_tr_cm(&WL, S0_ti_T_ti_ST);
-	co_di_eq_re(&WL, SUN_N);
-	return WL;
+	} else if (level == 1) {
+
+		for (int conf = 1; conf <= level_config_num[level]; ++conf) {
+			std::vector<int> curr_tag(conf_tag);
+			curr_tag.push_back(conf);
+
+			double* level2_T_fields[3];
+			for (int i = 0; i < 3; ++i)
+				T_field_alloc_zero(level2_T_fields[i], 3, T / level_thickness[2], L);
+			multilevel(curr_tag, level_thickness, level + 1, WL_R, level2_T_fields);
+
+			for (int t = 0; t < level_thickness[0]; t += level_thickness[level]) {
+				for (int x = 0; x < L; ++x) {
+					for (int y = 0; y < L; ++y) {
+						for (int z = 0; z < L; ++z) {
+							for (int i = 0; i < 3; ++i) {
+								int curr_index = T_field_index(t, x, y, z, 3, i, T, L, level_thickness[level]);
+								int sub_index_first = T_field_index(t, x, y, z, 3, i, T, L, level_thickness[level + 1]);
+								int sub_index_second = T_field_index(t + level_thickness[level + 1], x, y, z, 3, i, T, L,
+										level_thickness[level + 1]);
+								double U[2][SO_elems];
+
+								so_eq_so_ti_so(U[0], level2_T_fields[0] + sub_index_first, level2_T_fields[1] + sub_index_second);
+								so_eq_so_ti_so(U[1], level2_T_fields[2] + sub_index_first, level2_T_fields[0] + sub_index_second);
+
+								for (int j = 0; j < 2; ++j)
+									so_pl_eq_so(T_fields[j] + curr_index, U[j]);
+							}
+						}
+					}
+				}
+			}
+			for (int i = 0; i < 3; ++i)
+				delete[] level2_T_fields[i];
+		}
+		for (int i = 0; i < 2; ++i)
+			T_field_di_eq_re(T_fields[i], level_config_num[level], 3, T, L, level_thickness[level]);
+
+	} else if (level == 2) {
+		for (int conf = 0; conf < level_config_num[level]; ++conf) {
+			std::vector<int> curr_tag(conf_tag);
+			curr_tag.push_back(conf);
+
+			double* sub_gauge_field;
+			obtain_sublattice_gauge_field(sub_gauge_field, curr_tag, level_thickness[0], L);
+
+			for (int t = 0; t < T; t += level_thickness[level]) {
+				for (int x = 0; x < L; ++x) {
+					for (int y = 0; y < L; ++y) {
+						for (int z = 0; z < L; ++z) {
+							for (int i = 1; i < 4; ++i) {
+								int curr_index = T_field_index(t, x, y, z, 3, i - 1, T, L, level_thickness[level]);
+								double U[3][SO_elems];
+								compute_T_ti_T(U[0], sub_gauge_field, t, x, y, z, i, WL_R);
+								compute_T_ti_Tclov_lower_half(U[1], sub_gauge_field, t, x, y, z, i, WL_R);
+								compute_Tclov_upper_half_ti_T(U[2], sub_gauge_field, t, x, y, z, i, WL_R);
+
+								for (int j = 0; j < 3; ++j)
+									so_pl_eq_so(T_fields[j] + curr_index, U[j]);
+							}
+						}
+					}
+				}
+			}
+			Gauge_Field_Free(&sub_gauge_field);
+		}
+		for (int i = 0; i < 3; ++i)
+			T_field_di_eq_re(T_fields[i], level_config_num[level], 3, T, L, level_thickness[level]);
+	}
+
 }
 
 void handle_GNU_options(int argc, char**& argv) {
@@ -164,10 +171,10 @@ void handle_GNU_options(int argc, char**& argv) {
 
 int main(int argc, char **argv) {
 	using namespace std;
-	int WL_r = 5, WL_T = 8;
+	int WL_R = 5, WL_T = 8;
 
 	if (argc != 6) {
-		cerr << "Usage: " << argv[0] << "[-E | --Ez] <T> <L> <level_config_num> <config_prefix> <config_id>\n";
+		cerr << "Usage: " << argv[0] << " [-E | --Ez] <T> <L> <level_config_num> <config_prefix> <config_id>\n";
 		return 0;
 	}
 
@@ -195,34 +202,36 @@ int main(int argc, char **argv) {
 	Gauge_Field_Alloc(&gauge_field, T, L);
 	read_gauge_field(gauge_field, config_filename( { config_lv0_id }).c_str(), T, L);
 
+	const std::vector<int> level_thickness { T, 4, 2 };
+	const int timeslice_num = level_thickness[0] / level_thickness[1];
+
+	double** T_field;
+	T_field_alloc_zero(*T_field, 3, timeslice_num, L);
+
+	multilevel( { config_lv0_id }, level_thickness, 0, WL_R, T_field);
+
 	complex WL_avg;
 	co_eq_zero(&WL_avg);
-
-	for (int t = 0; t < T; t += 2) {
+	for (int t = 0; t < T; t += level_thickness[1]) {
 		for (int x = 0; x < L; ++x) {
 			for (int y = 0; y < L; ++y) {
 				for (int z = 0; z < L; ++z) {
-					std::array<int, 4> n = { t, x, y, z };
 					for (int i = 1; i < 4; ++i) {
-
-						double SO[SO_elems];
-						multilevel( { config_lv0_id }, WL_T, 0, 0, n, i, WL_r, SO);
-						LinkPath S0(gauge_field, T, L, n), ST(gauge_field, T, L, n);
-						ST.move(0, WL_T);
-						for (int r = 0; r < WL_r; ++r) {
+						LinkPath S0(gauge_field, T, L, { t, x, y, z }), ST(gauge_field, T, L, { t + WL_T, x, y, z });
+						for (int r = 0; r < WL_R; ++r) {
 							S0(i, true);
 							ST(i, true);
 						}
-
-						complex curr_WL = complete_Wilson_loop(SO, S0.path, ST.path);
+						complex curr_WL;
+						close_Wilson_loop(&curr_WL, *T_field + T_field_index(t, x, y, z, 3, i - 1, T, L, level_thickness[1]),
+								S0.path, ST.path);
 						co_pl_eq_co(&WL_avg, &curr_WL);
 					}
 				}
 			}
 		}
 	}
-
-	co_di_eq_re(&WL_avg, 3.0 * T / 2.0 * L * L * L);
+	co_di_eq_re(&WL_avg, 3.0 * L * L * L * timeslice_num);
 
 	cout << scientific << setprecision(11) << showpos << WL_avg.re << " " << WL_avg.im << "\n";
 }
