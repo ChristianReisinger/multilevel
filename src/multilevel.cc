c7bf8a1bbe043d62e630b05588ee69bb9d73746b
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-08-15 15:15:00 +0200

    Add option to print memory requirement

--- src/multilevel.cc
+++ src/multilevel.cc
@@ -22,19 +22,35 @@
 #include <twolink_operators.hh>
 #include <MultilevelAnalyzer.hh>
 
+bool show_mem = false;
+
 void handle_GNU_options(int argc, char**& argv) {
 	static struct option long_opts[] = {
+			{ "mem", no_argument, 0, 'm' },
 			{ 0, 0, 0, 0 }
 	};
 
 	int opt = -1, long_opts_i = 0;
-	while ((opt = getopt_long(argc, argv, "", long_opts, &long_opts_i)) != -1) {
+	while ((opt = getopt_long(argc, argv, "m", long_opts, &long_opts_i)) != -1) {
 		switch (opt) {
-			default:
-				break;
+			case 'm':
+				show_mem = true;
+			break;
 		}
 	}
 	argv = argv + optind - 1;
+}
+
+double memory_bytes_used(std::vector<std::vector<std::vector<int> > > field_compositions, std::vector<int> level_thickness, int T,
+		int L) {
+	int levels = level_thickness.size();
+	if (levels < 2 || field_compositions.size() != levels)
+		return -1;
+	double bytes_per_field = T * L * L * L * 3 * SUN_N * SUN_N * SUN_N * SUN_N * 2 * 8;
+	double operators_per_site = 0.0;
+	for (int level = 0; level < levels; ++level)
+		operators_per_site += (double) field_compositions[level].size() / (double) level_thickness[level == 0 ? 1 : level];
+	return operators_per_site * bytes_per_field;
 }
 
 int main(int argc, char **argv) {
@@ -43,7 +59,7 @@ int main(int argc, char **argv) {
 	int T_offset = 1; // T_field computed with multilevel at site n corresponds to a temporal Wilson line direct product at site n + T_offset * unit_vec_t
 
 	if (argc != 7) {
-		cerr << "Usage: " << argv[0] << " <T> <L> <level_config_num> <config_prefix> <config_id> <outfile>\n";
+		cerr << "Usage: " << argv[0] << " [--mem | -m] <T> <L> <level_config_num> <config_prefix> <config_id> <outfile>\n";
 		return 0;
 	}
 
@@ -82,6 +98,13 @@ int main(int argc, char **argv) {
 			{ { 0, 1 }, { 2, 1 }, { 3 } },
 			{ { 0 }, { 1 }, { 2 }, { 3 } }
 	};
+
+	if (show_mem) {
+		cout << "This computation uses " << memory_bytes_used(field_compositions, level_thickness, T, L) / 1000000.0 << " MB memory.\n";
+		return 0;
+	}
+
+//	***************************************************************************************************************************************
 
 	MultilevelAnalyzer multilevel(T, L, WL_R, level_thickness, argv[4], level_config_num,
 			{ IU_x_IU, UU_x_UU, UU_x_UCU, UI_x_UI }, field_compositions);
