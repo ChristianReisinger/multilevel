472dd3a9ce29aa689de934b1b3d4ae354210449d
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-10-16 19:51:15 +0200

    Add class TwolinkOperator

---
+++ include/TwolinkOperator.hh
@@ -0,0 +1,48 @@
+#include <vector>
+#include <string>
+#include <map>
+#include <set>
+#include <memory>
+
+#include <OperatorFactor.hh>
+#include <T_field.hh>
+
+#ifndef INCLUDE_DE_UNI_FRANKFURT_ITP_REISINGER_MULTILEVEL_0819_TWOLINKOPERATOR_HH_
+#define INCLUDE_DE_UNI_FRANKFURT_ITP_REISINGER_MULTILEVEL_0819_TWOLINKOPERATOR_HH_
+
+namespace de_uni_frankfurt_itp {
+namespace reisinger {
+namespace multilevel_0819 {
+
+class TwolinkOperator: OperatorFactor {
+public:
+	TwolinkOperator(std::string name, std::vector<bool> timeslice_isdefined, std::vector<OperatorFactor*> factors);
+
+	std::string descr() const;
+	std::string descr(std::string str);
+
+	std::string name() const override;
+	int t_extent() const override;
+	void at(double* result, int t, int x, int y, int z, int dir, int rsep,
+			const double* sub_gauge_field = nullptr, int T = 0, int L = 0) const override;
+
+	void alloc_T_fields(const std::set<int>& WL_Rs, const std::vector<int>& timeslice_sizes, int T, int L);
+
+private:
+	std::string m_name, m_descr = "";
+	std::vector<bool> m_timeslice_isdefined;
+	std::vector<OperatorFactor*> m_factors;
+
+	std::string m_name;
+	int m_t_extent;
+	std::map<int, T_field> m_r_fields { };
+
+	bool valid_timeslice_def();
+
+};
+
+}
+}
+}
+
+#endif
---
+++ src/TwolinkOperator.cc
@@ -0,0 +1,87 @@
+#include <vector>
+#include <stdexcept>
+#include <utility>
+
+#include <OperatorFactor.hh>
+#include <T_field.hh>
+#include <sublattice_algebra.hh>
+
+#include <TwolinkOperator.hh>
+
+namespace de_uni_frankfurt_itp {
+namespace reisinger {
+namespace multilevel_0819 {
+
+TwolinkOperator::TwolinkOperator(std::string name, std::vector<bool> timeslice_isdefined,
+		std::vector<OperatorFactor*> factors) :
+		m_name(name), m_timeslice_isdefined(m_timeslice_isdefined), m_factors(factors), m_t_extent(0) {
+
+	if (name.empty())
+		throw std::invalid_argument("invalid LevelOperator");
+
+	if (!valid_timeslice_def())
+		throw std::invalid_argument("LevelOperator not defined on any timeslice");
+
+	for (const OperatorFactor* def : factors)
+		m_t_extent += def->t_extent();
+
+}
+
+std::string TwolinkOperator::descr() const {
+	return m_descr;
+}
+
+std::string TwolinkOperator::descr(std::string str) {
+	m_descr = str;
+	return descr();
+}
+std::string TwolinkOperator::name() const {
+	return m_name;
+}
+
+int TwolinkOperator::t_extent() const {
+	return m_t_extent;
+}
+
+void TwolinkOperator::at(double* result, int t, int x, int y, int z, int dir, int rsep,
+		const double* sub_gauge_field, int T, int L) const {
+
+	if (sub_gauge_field != nullptr || T != 0 || L != 0)
+		throw std::invalid_argument("invalid T_field access");
+
+	try {
+		so_eq_so(result, m_r_fields.at(rsep).T_at(t, x, y, z, dir));
+	} catch (std::out_of_range& e) {
+		throw std::runtime_error("cannot read LevelOperator " + m_name);
+	}
+
+}
+
+void TwolinkOperator::alloc_T_fields(const std::set<int>& WL_Rs,
+		const std::vector<int>& timeslice_sizes, int T, int L) {
+
+	std::vector<std::pair<int, bool> > timeslice_size_defined;
+	if (timeslice_sizes.size() != m_timeslice_isdefined.size())
+		throw std::invalid_argument("invalid timeslice definitions");
+	for (int tsl = 0; tsl < timeslice_sizes.size(); ++tsl)
+		timeslice_size_defined.emplace_back(timeslice_sizes[tsl], m_timeslice_isdefined[tsl]);
+
+	for (int WL_R : WL_Rs)
+		m_r_fields.emplace(WL_R, T_field(timeslice_size_defined, T, L));
+}
+
+//private
+
+bool TwolinkOperator::valid_timeslice_def() {
+	bool is_def = false;
+	for (bool b : m_timeslice_isdefined)
+		if (b) {
+			is_def = true;
+			break;
+		}
+	return is_def;
+}
+
+}
+}
+}
