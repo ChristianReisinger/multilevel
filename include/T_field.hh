6b030370e1fbcca4b56d11dde716d5c7ec2e0a76
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2019-10-19 14:48:02 +0200

    Rename member variables to m_..

--- include/T_field.hh
+++ include/T_field.hh
@@ -3,7 +3,6 @@
 #include <utility>
 #include <map>
 
-#include <geometry.hh>
 #include <sublattice_algebra.hh>
 
 #ifndef INCLUDE_T_FIELD_HH_
@@ -25,24 +24,24 @@ public:
 
 	std::set<int> defined_ts() const;
 	double* T_at(int t, int x, int y, int z, int dir) const;
-	inline int timeslice_thickness() const {
-		return thickness;
+	inline int timeslice_size() const {
+		return m_timeslice_size;
 	}
 	T_field& operator/=(double d);
 
 private:
 
 	inline int field_elems() {
-		return t_to_index.size() * L * L * L * 3 * SO_elems;
+		return m_t_to_index.size() * m_L * m_L * m_L * 3 * SO_elems;
 	}
 
-	double* data;
-	int thickness, T, L;
-	std::map<int, int> t_to_index;
+	double* m_data;
+	int m_timeslice_size, m_T, m_L;
+	std::map<int, int> m_t_to_index;
 };
 
 }
 }
 }
 
-#endif /* INCLUDE_T_FIELD_HH_ */
+#endif
