bde9c82ff2c47dca3a3d9bc39310e279d6704c65
Christian Reisinger <reisinger@itp.uni-frankfurt.de>
2020-02-11 19:21:37 +0100

    Implement functions to find & print operator placements

--- src/bin/try_operator_on_timeslices.cc
+++ src/bin/try_operator_on_timeslices.cc
@@ -6,6 +6,7 @@
 #include <numeric>
 
 #include <helper_functions.hh>
+#include <io_tools.hh>
 
 namespace de_uni_frankfurt_itp {
 namespace reisinger {
@@ -15,6 +16,10 @@ namespace try_operator_on_timeslices {
 using t_coord = int;
 using t_extent = int;
 using timeslice_sizes = std::vector<int>;
+
+enum observable_mode {
+	force
+};
 
 void print_syntax_help(char* argv0) {
 	std::cout << "\nUsage: " << argv0 << ""
@@ -93,15 +98,15 @@ bool is_timeslice_boundary(int t, const timeslice_sizes& timeslice_sizes_pattern
 }
 
 /**
- * @param operator_t_extent	size of the operator in temporal direction
- * @param no_boundary_times	for each set in no_boundary_times, at least of the times in that set must
- * 							not be a timeslice boundary (i.e. elements in the outer set are related by
- * 							&&, elements in the inner sets by ||)
+ * @param operator_t_extent					size of the operator in temporal direction
+ * @param boundary_forbidden_time_coords	for each set in no_boundary_times, at least of the times in that set must
+ * 											not be a timeslice boundary (i.e. elements in the outer set are related by
+ * 											&&, elements in the inner sets by ||)
  * @return	set of time coordinates where an operator can be placed to start and end on a timeslice
  * 			boundary, and fulfill the required conditions given by the parameters
  */
-std::set<t_coord> find_possible_operator_time_coords(t_extent operator_t_extent,
-		const timeslice_sizes& timeslice_sizes_pattern, const std::set<std::set<int> >& no_boundary_times) {
+auto find_possible_operator_time_coords(t_extent operator_t_extent,
+		const timeslice_sizes& timeslice_sizes_pattern, const std::set<std::set<int> >& boundary_forbidden_time_coords) {
 
 	std::set<t_coord> possible_times;
 
@@ -109,7 +114,7 @@ std::set<t_coord> find_possible_operator_time_coords(t_extent operator_t_extent,
 			operator_pos_t = next_timeslice_boundary(operator_pos_t, timeslice_sizes_pattern)) {
 
 		bool non_boundaries_fulfilled = true;
-		for (const int one_of_t_not_a_boundary : no_boundary_times) {
+		for (const auto& one_of_t_not_a_boundary : boundary_forbidden_time_coords) {
 			bool found_non_boundary = false;
 			for (const int t : one_of_t_not_a_boundary) {
 				if (!is_timeslice_boundary(operator_pos_t + t, timeslice_sizes_pattern)) {
@@ -130,16 +135,71 @@ std::set<t_coord> find_possible_operator_time_coords(t_extent operator_t_extent,
 	return possible_times;
 }
 
-void try_place_operator(const timeslice_sizes& timeslice_sizes_pattern, const std::set<t_extent> t_extents,
-		std::map<timeslice_sizes, std::map<t_extent, std::vector<t_coord> > >& possible_operator_placements) {
+auto get_boundary_forbidden_time_coords(t_extent operator_t_extent, observable_mode mode) {
 
-	std::set<std::set<int> > no_boundary_times;
-	//TODO construct no_boundary_times
+	std::set<std::set<t_coord> > boundary_forbidden_t_coords;
+
+	switch (mode) {
+		case force:
+			std::set<t_extent> center_coords = { operator_t_extent / 2 };
+			if (operator_t_extent % 2 != 0)
+				center_coords.insert(operator_t_extent / 2 + 1);
+			boundary_forbidden_t_coords.insert(center_coords);
+			break;
+	}
+	return boundary_forbidden_t_coords;
+}
+
+auto try_place_operator(const timeslice_sizes& timeslice_sizes_pattern,
+		const std::set<t_extent>& t_extents, observable_mode mode) {
+
+	std::map<t_extent, std::set<t_coord> > possible_extents;
 
 	for (const t_extent operator_t_extent : t_extents) {
 		std::set<t_coord> possible_operator_t_coords = find_possible_operator_time_coords(operator_t_extent,
-				timeslice_sizes_pattern, no_boundary_times);
-		//TODO store results in possible_operator_pacements
+				timeslice_sizes_pattern, get_boundary_forbidden_time_coords(operator_t_extent, mode));
+
+		if (!possible_operator_t_coords.empty())
+			possible_extents[operator_t_extent] = possible_operator_t_coords;
+	}
+
+	return possible_extents;
+}
+
+void scan_operator_placements(const timeslice_sizes& timeslice_sizes_pattern,
+		const std::set<t_extent>& t_extents, observable_mode mode,
+		std::map<timeslice_sizes, std::map<t_extent, std::set<t_coord> > >& possible_placements) {
+
+	auto possible_extents = try_place_operator(timeslice_sizes_pattern, t_extents, mode);
+	if (!possible_extents.empty())
+		possible_placements[timeslice_sizes_pattern] = possible_extents;
+}
+
+void filter(std::map<timeslice_sizes, std::map<t_extent, std::set<t_coord> > >& operator_placements,
+		const std::set<t_extent>& required_extents) {
+
+	for (auto it = operator_placements.begin(); it != operator_placements.end();) {
+		bool has_required = true;
+		for (t_extent ext : required_extents)
+			if (!it->second.count(ext)) {
+				has_required = false;
+				break;
+			}
+		if (has_required)
+			++it;
+		else
+			it = operator_placements.erase(it);
+	}
+}
+
+void print(const std::map<timeslice_sizes, std::map<t_extent, std::set<t_coord> > >& operator_placements) {
+	using tools::io_tools::operator<<;
+
+	for (const auto& pattern : operator_placements) {
+		std::cout << pattern.first << " : ";
+		for (const auto& t_extent : pattern.second)
+			std::cout << t_extent.first << " " << t_extent.second << " ";
+		std::cout << "\n";
 	}
 }
 
@@ -155,30 +215,19 @@ int main(int argc, char** argv) {
 	using tools::helper::parse_unsigned_int_list;
 	using tools::helper::nest_for;
 
-	//TODO change program syntax, parse new parameters
-	const int tsl_num = 2, min_size = 1, max_size = 3;
-	const set<int> ts = { 4, 5, 6 };
+	/*TODO change program syntax, parse new parameters
+	 * options: -f <required_extents>
+	 */
+	const int tsl_num = 4, min_size = 1, max_size = 3;
+	const set<t_extent> t_extents = { 4, 5, 6 };
 	const vector<pair<int, int> > size_limits(tsl_num, { min_size, max_size + 1 });
 
-	map<timeslice_sizes, map<t_extent, vector<t_coord> > > possible_operator_placements;
+	map<timeslice_sizes, map<t_extent, set<t_coord> > > possible_operator_placements;
 	nest_for(size_limits,
-			try_place_operator, ts, possible_operator_placements);
+			scan_operator_placements, t_extents, force, possible_operator_placements);
 
-	//TODO report results
+	filter(possible_operator_placements, t_extents);
 
-/*
-	vector<int> no_boundary_times;
-	if (!handle_GNU_options(argc, argv, no_boundary_times))
-		return 0;
-
-	const vector<int> timeslice_sizes = parse_unsigned_int_list(argv[1]);
-	const int t_extent = stoi(argv[2]);
-
-	set<int> possible_ts = find_possible_operator_time_coords(t_extent, timeslice_sizes, no_boundary_times);
-	cout << possible_ts.size() << " possible positions: ";
-	for (const int t : possible_ts)
-		cout << t << " ";
-	cout << "\n";
-	*/
+	print(possible_operator_placements);
 
 }
